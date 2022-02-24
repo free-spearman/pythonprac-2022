53b0b69ee82790d889107586bfebe5fb4434a47c
Ilya Badekin <link.to.badekin@gmail.com>
2022-02-24 20:52:14 +0300

    Hometask from 14.02.2022: Step 3
    Добавлено:
    -вывод дерева, на который указывает последний коммит выбранной ветки.
    -проверка на существование ветки
    
    Изменино:
    Имена констант PATHGITBRANCHES -> PATH_GIT_BRANCHES, PATHGITOBJECTS -> PATH_GIT_OBJECTS

--- 20220214/1/gitdir.py
+++ 20220214/1/gitdir.py
@@ -1,9 +1,11 @@
 import sys, zlib
 from os import listdir
-from os.path import join
+from os.path import join, exists
 
-PATHGITBRANCHES = '.git/refs/heads/'
-PATHGITOBJECTS = '.git/objects/'
+PATH_GIT_BRANCHES = '.git/refs/heads/'
+PATH_GIT_OBJECTS = '.git/objects/'
+BYTE_SEP = b'\x00'
+SHIFT = "  "
 
 def git_branches_show (*branch_name):
     if not branch_name:
@@ -11,19 +13,41 @@ def git_branches_show (*branch_name):
             print(branch)
         return    
     
-    branch_path = join(PATHGITBRANCHES,branch_name[0]) 
+    branch_path = join(PATH_GIT_BRANCHES,branch_name[0]) 
+    if  not exists(branch_path):
+        print(f"Нет такой ветки или пути {branch_path}")
+        return
+
     with open(branch_path) as f_branch:
         f_text = f_branch.readline()
     
     f_text = f_text.strip()
     commit_name_dir, commit_name_file = f_text[:2] +'/', f_text[2:]
-    commit_file_path = join(PATHGITOBJECTS,commit_name_dir,commit_name_file)
+    commit_file_path = join(PATH_GIT_OBJECTS,commit_name_dir,commit_name_file)
     
     with open(commit_file_path, 'rb') as obj_file:
         commit = zlib.decompress(obj_file.read()).decode()
     
     print(commit)
 
+    commit_tree_name = commit.split('\n')[0].split('tree')[1].strip()
+    tree_dir, tree_file = commit_tree_name[:2]+'/', commit_tree_name[2:]
+    tree_file_path = join(PATH_GIT_OBJECTS,tree_dir,tree_file)
+
+    with open(tree_file_path, 'rb') as f_tree:
+        f_text = zlib.decompress(f_tree.read())
+    tail = f_text.partition(BYTE_SEP)[-1]
+    
+    while tail:
+        head, _, tail = tail.partition(BYTE_SEP)
+        tre_mode, tre_name = head.split()
+        num, tail = tail[:20], tail[20:]
+        print(f"{SHIFT}{tre_name.decode()} {tre_mode.decode()} {num.hex()}")
+     
+
+
+
+    
 
 if ( __name__ == '__main__'):
     if len(sys.argv) > 1:
