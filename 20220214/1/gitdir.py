75bd19bbe35b50d5f1a67931bb05c2bf5d13dda0
Ilya Badekin <link.to.badekin@gmail.com>
2022-02-24 19:33:40 +0300

    Hometask from 14.02.2022: Step 2
    Добавлена функция: при указании в качестве параметра командной строки или функции имени ветки выводится содержимое объекта-последнего коммита данной ветки. Файл 20220214/1/gitdir.py

--- 20220214/1/gitdir.py
+++ 20220214/1/gitdir.py
@@ -1,12 +1,39 @@
+import sys, zlib
 from os import listdir
+from os.path import join
 
 PATHGITBRANCHES = '.git/refs/heads/'
+PATHGITOBJECTS = '.git/objects/'
+
+def git_branches_show (*branch_name):
+    if not branch_name:
+        for branch in listdir(PATHGITBRANCHES):
+            print(branch)
+        return    
+    branch_path = join(PATHGITBRANCHES,branch_name[0])
+    print(branch_path)    
+    with open(branch_path) as f_branch:
+        f_text = f_branch.readline()
+    print(f_text)
+    f_text = f_text.strip()
+    commit_name_dir, commit_name_file = f_text[:2] +'/', f_text[2:]
+    commit_file_path = join(PATHGITOBJECTS,commit_name_dir,commit_name_file)
+    print(commit_file_path)
+    with open(commit_file_path, 'rb') as obj_file:
+        commit = zlib.decompress(obj_file.read()).decode()
+    print(commit)
 
 
-def git_branches_show ():
-    for branch in listdir(PATHGITBRANCHES):
-        print(branch)
+
+
+
+
+        
+
 
 
 if ( __name__ == '__main__'):
-    git_branches_show()
+    if len(sys.argv) > 1:
+        git_branches_show(sys.argv[1])
+    else:
+        git_branches_show()
