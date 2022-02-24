9278a3e230dfe8ff4d2b5f6b61f43f98a243f38a
Ilya Badekin <link.to.badekin@gmail.com>
2022-02-24 18:27:31 +0300

    Hometask from 14.02.2022: Step 1
    def git_branches_show or file gitdir.py outputs the names of all branches

--- 20220214/1/gitdir.py
+++ 20220214/1/gitdir.py
@@ -1,22 +1,12 @@
-import zlib
-import sys
-from glob import iglob
-from os.path import join
+from os import listdir
 
-repo = sys.argv[1]
-repopath = join(repo, "objects", "??", "*")
-for objname in iglob(repopath):
-    print(objname)
-        
-    with open(objname, "rb") as objfile:
-        fullobj = zlib.decompress(objfile.read())
-        header, _, body = fullobj.partition(b'\x00')
-        objkind, size = header.split()
-        print(objkind.decode(), int(size))
-        if objkind == b'commit':
-            print(body.decode())
-        elif objkind == b'tree':
-            while(body):
-               treehdr, _, tail = body.partition(b'\x00')
-               gitid, body = tail[:20], tail[20:]
-               print(f'\t{treehdr.decode()} {gitid.hex()}')
+PATHGITBRANCHES = '.git/refs/heads/'
+
+
+def git_branches_show ():
+    for branch in listdir(PATHGITBRANCHES):
+        print(branch)
+
+
+if ( __name__ == '__main__'):
+    git_branches_show()
