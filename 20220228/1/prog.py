a25cc2992a67e64a806a587bdd3092dbf9d23c1a
Ilya Badekin <link.to.badekin@gmail.com>
2022-03-01 14:21:26 +0300

    Home 28.02.22 commit 2.2
    Поддержка в функции dist третьего параметра, передача третьей строки в качестве этого параметра

--- 20220228/1/prog.py
+++ 20220228/1/prog.py
@@ -1,7 +1,7 @@
 import textdistance as txtdist
 MESS = "Строка без пробела \n"
 
-def dist(s1, s2):
+def dist(s1, s2, type):
     return txtdist.levenshtein(s1, s2)
 
 
@@ -11,4 +11,4 @@ if ( __name__ == '__main__'):
 	str2 = input(MESS).replace(" ",'')
 	str3 = input(MESS).replace(" ",'')
 
-	res = dist(str1, str2)
+	res = dist(str1, str2, str3)
