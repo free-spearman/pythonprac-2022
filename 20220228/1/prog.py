bab023c94471f17b7fd8195e0092f51e262da430
Ilya Badekin <link.to.badekin@gmail.com>
2022-03-01 13:33:41 +0300

    Home 28.02.22 commit 2
    чтение со стандартного ввода двух строк, не содержащих пробелы (если есть, то удаляем)

--- 20220228/1/prog.py
+++ 20220228/1/prog.py
@@ -1,5 +1,11 @@
 import textdistance as txtdist
+MESS = "Строка без пробела \n"
 
 def dist(s1, s2):
     return txtdist.levenshtein(s1, s2)
- 
+
+
+if ( __name__ == '__main__'):
+	
+	str1 = input(MESS).replace(" ",'')
+	str2 = input(MESS).replace(" ",'')
