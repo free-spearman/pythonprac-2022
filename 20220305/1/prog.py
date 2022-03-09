ca711f3ef440a61be578e7a8cd6dd0d4b1306309
Ilya Badekin <link.to.badekin@gmail.com>
2022-03-09 20:14:04 +0300

    Добавлено 3 класса
    class GameObj - шаблон для  игровых объектов
    	-__init__
    	-__str__
    	-take_damage
    	-Поля: hp, coords, name
    
    class Monster(GameObj) - класс для монстра
    	-добавлено поле type = MONSTER
    
    class GameMap - игровая карта
    	-__init__
    	-check_coord_exit - проверка координат
    	-add_obj - добавить объектов на карту

---
+++ 20220305/1/prog.py
@@ -0,0 +1,77 @@
+import readline, shlex, cmd 
+
+# Блок констант
+Y_AXIS = 0
+X_AXIS = 1
+Y_SIZE = 10
+X_SIZE = 10
+MOVE_ERROR_MESS = " Попытка выйти за границу мира"    
+MAP_SIZE =[Y_SIZE,X_SIZE]
+MONSTER = 'MONSTER'
+HP = 'HP'
+CRDS = 'COORDS'
+NAME = 'NAME'
+
+# Блок констант
+
+
+class GameObj:
+    """ Class GameObj. Родительский класс для игровых объектов"""
+    def __init__(self, args):
+        self.hp = int(args[HP])
+        self.coords = args[CRDS]
+        self.name = args[NAME]
+    def __str__(self):
+        result = f"{self.name} at {self.coords} hp {self.hp}"
+        return result
+    def take_damage(self, dmg):
+        """ если убили - False, иначе hp"""
+        self.hp = self.hp - dmg
+        if self.hp < 1:
+            return False
+        else: 
+            return self.hp  
+
+class Monster(GameObj):
+    """ Class Monster, наследник GameObj"""
+    def __init__(self, args):
+        super().__init__(args)
+        self.type = MONSTER
+
+
+class GameMap:
+    """ карта """
+    def __init__(self):
+        self.map = [[False for _ in range(X_SIZE)] for _ in range(Y_SIZE)]
+        print()
+        self.player_coords = (0, 0)
+        print('Player at', *(self.player_coords))
+        self.monsters = []
+        
+    
+    def check_coord_exit(axis,coord):
+        """Вышли за границу - True, иначе False"""
+        if coord < 0 or coord > MAP_SIZE[axis] - 1:
+            return True
+        else:
+            return False    
+    
+    def move(self, axis, step):
+        """Return true if everything is bad, false - moving successfully"""
+        new_coord = self.player_coords[axis] + step
+        if check_cord_exit(axis ,new_coord):
+            print (MOVE_ERROR_MESS)
+            return True
+        self.player_coords[axis] = new_coord
+        return False
+
+    def add_obj(self, coords, obj):
+        """добавляет объект на карту"""
+        if check_coord_exit(Y_AXIS,coords[Y_AXIS]) or check_coord_exit(X_AXIS,coords[X_AXIS]):
+            print (MOVE_ERROR_MESS)
+            return True
+        field = self.map[coords[Y_AXIS]][coords[X_AXIS]]
+        if not field:
+            field = [obj]
+            return
+        self.map[coords[Y_AXIS]][coords[X_AXIS]] = field.append(obj)
